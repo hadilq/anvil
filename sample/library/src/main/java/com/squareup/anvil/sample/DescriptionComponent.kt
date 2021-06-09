e75f4faf033d5850f0c7ef63ab37611a04f8d310
Hadi <hadilashkari@gmail.com>
2021-06-09 16:51:02 -0400

    Add map multibinding to the sample

--- sample/library/src/main/java/com/squareup/anvil/sample/DescriptionComponent.kt
+++ sample/library/src/main/java/com/squareup/anvil/sample/DescriptionComponent.kt
@@ -3,10 +3,12 @@ package com.squareup.anvil.sample
 import com.squareup.anvil.annotations.ContributesTo
 import com.squareup.anvil.sample.father.FatherProvider
 import com.squareup.anvil.sample.mother.MotherProvider
+import com.squareup.anvil.sample.pet.Pet
 import com.squareup.scopes.AppScope
 
 @ContributesTo(AppScope::class)
 interface DescriptionComponent {
   fun fatherProvider(): FatherProvider
   fun motherProvider(): MotherProvider
+  fun petMap(): Map<Class<out God>, @JvmSuppressWildcards Pet>
 }
